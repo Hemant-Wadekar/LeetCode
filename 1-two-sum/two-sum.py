5c1f37baacf5147a6540a89ca87eedfec07c6bd8
Hemant Wadekar <hemantwadekar229@gmail.com>
2026-08-19 09:49:07 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (82.24%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        a = {}
+        for i,num in enumerate(nums):
+            complement = target - nums[i]
+            if complement in a:
+                return[a[complement],i]
+            a[num] = i
+        return []
